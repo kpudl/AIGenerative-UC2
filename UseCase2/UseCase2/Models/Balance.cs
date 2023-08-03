0d54cb6ebc010fefb35d0f5846265a43f0007f9a
kpudl <kpudl@softserveinc.com>
2023-08-03 11:30:20 +0200

    UC#2 Create list balance endpoint

---
+++ UseCase2/UseCase2/Models/Balance.cs
@@ -0,0 +1,9 @@
+﻿namespace UseCase2.Models
+{
+    public class Balance
+    {
+        public Dictionary<string, long> AvailableFunds { get; set; }
+
+        public Dictionary<string, long> PendingFunds { get; set; }
+    }
+}
