1924b0db3c3fd61a79bc32e369bde226e6ab6ed4
kpudl <kpudl@softserveinc.com>
2023-08-03 12:11:12 +0200

    UC#2 Create list balance transactions endpoint

---
+++ UseCase2/UseCase2/Models/BalanceTransaction.cs
@@ -0,0 +1,19 @@
+﻿namespace UseCase2.Models
+{
+    public class BalanceTransaction
+    {
+        public string Id { get; set; }
+
+        public float Amount { get; set; }
+
+        public string Currency { get; set; }
+
+        public float NettoAmount { get; set; }
+
+        public string Status { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
