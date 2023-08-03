3b53d829696080a71fb78d4b13b6ddbe999735f4
kpudl <kpudl@softserveinc.com>
2023-08-03 12:30:10 +0200

    merge

--- UseCase2/UseCase2/Models/Balance.cs
+++ UseCase2/UseCase2/Models/Balance.cs
@@ -2,14 +2,8 @@
 {
     public class Balance
     {
-<<<<<<< HEAD
         public Dictionary<string, float> AvailableFunds { get; set; }
 
         public Dictionary<string, float> PendingFunds { get; set; }
-=======
-        public Dictionary<string, long> AvailableFunds { get; set; }
-
-        public Dictionary<string, long> PendingFunds { get; set; }
->>>>>>> 0d54cb6 (UC#2 Create list balance endpoint)
     }
 }
